dafdf84c390c361ddb054efc3dcd916190ca9a89
Mateusz Kowalski <mateusz.kowalski@cocoders.com>
2019-03-01 18:41:17 +0100

    Add files via upload

--- zagadka.js
+++ zagadka.js
@@ -1,72 +1,46 @@
-const temperatureSlider = document.getElementById('temperatureSlider');
-const temperatureDisplay = document.getElementById('temperatureDisplay');
-const oxygeneSlider = document.getElementById('oxygeneSlider');
-const oxygeneDisplay = document.getElementById('oxygeneDisplay');
-const pressureSlider = document.getElementById('pressureSlider');
-const pressureDisplay = document.getElementById('pressureDisplay');
-const gravitySlider = document.getElementById('gravitySlider');
-const gravityDisplay = document.getElementById('gravityDisplay');
+// const temperatureSlider = document.getElementById('temperatureSlider');
+
+// const oxygeneSlider = document.getElementById('oxygeneSlider');
+
+// const pressureSlider = document.getElementById('pressureSlider');
+// const gravitySlider = document.getElementById('gravitySlider');
+
 //poprawne odpowiedzi do zagadek
-const correctTemperatureValue = 9;
-const correctOxygeneValue = 24;
-const correctPressureValue = 1002;
-const correctGravityValue = 0.8;
-//ręczne ustawienie elementu po odpaleniu strony
-// const placeElements = () => {
-//     temperatureDisplay.style.transform = `translateX(${(temperatureSlider.value - temperatureSlider.min)/(temperatureSlider.max-temperatureSlider.min)*(temperatureSlider.offsetWidth - temperatureDisplay.offsetWidth)}px)`;
-// }
-// setTimeout(placeElements, 0);
-// Nieudana próba napisania jednej funckji dla wszystkich suwaków 
-// const displayValue = (m, s) => {
-//     const sliderPosition = (s.value - s.min) / (s.max - s.min);
-//     console.log(sliderPosition);
-//     m.textContent = s.value;
-//     m.style.transform = `translateX(${sliderPosition*(s.offsetWidth - m.offsetWidth)}px)`
-// }
-// suwakTemperatury.addEventListener('input', displayValue(valueTemperatury, suwakTemperatury))
-const displayTemperatureValue = (temperatureDisplayP, temperatureSliderP) => {
-    const marker = temperatureDisplayP;
-    const slider = temperatureSliderP;
+
+const displayValue = (display, slider) => {
     const sliderPosition = (slider.value - slider.min) / (slider.max - slider.min);
-    console.log(sliderPosition);
-    marker.textContent = slider.value;
-    marker.style.transform = `translateX(${sliderPosition*(slider.offsetWidth - marker.offsetWidth)}px)`
+    display.textContent = slider.value;
+    display.style.transform = `translateX(${sliderPosition*(slider.offsetWidth - display.offsetWidth)}px)`
 }
 
-temperatureSlider.addEventListener('input', function () {
-    displayTemperatureValue(temperatureDisplay, temperatureSlider);
-});
+const sliders = document.querySelectorAll('input');
+const puzzleTrigger = (element) => {
+    if (element.value !== element.dataset.correct) {
+        return;
+    }
 
-oxygeneSlider.addEventListener('input', function () {
-    displayTemperatureValue(oxygeneDisplay, oxygeneSlider);
-});
+    let result = true;
+    for (let i = 0; i < sliders.length; i++) {
+        let currentSlider = sliders[i];
 
-pressureSlider.addEventListener('input', function () {
-    displayTemperatureValue(pressureDisplay, pressureSlider);
-});
+        if (currentSlider.value !== currentSlider.dataset.correct) {
+            result = false;
+            break;
+        }
+    }
 
-gravitySlider.addEventListener('input', function () {
-    displayTemperatureValue(gravityDisplay, gravitySlider);
-});
-
-// oxygeneSlider.addEventListener('input', displayTlenValue);
-// gravitySlider.addEventListener('input', displayGrawitacjaValue);
-//Pobranie wszystkich suwaków
-const suwaki = document.querySelectorAll('input');
-//Funkcja wyświetlająca button kończący grę
-const puzzleTrigger = () => {
-    const button = document.querySelector('button');
-    if ((temperatureSlider.value == correctTemperatureValue) && (oxygeneSlider.value == correctOxygeneValue) && (pressureSlider.value == correctPressureValue) && (gravitySlider.value == correctGravityValue)) {
+    if (result === true) {
+        const button = document.querySelector('button');
         console.log("zadanie wykonane");
         button.style.display = "block";
     }
 }
-//Nasłuchiwanie wszystkich suwaków 
-suwaki.forEach(suwak => {
-    suwak.addEventListener('change', puzzleTrigger)
+
+sliders.forEach(currentSlider => {
+    currentSlider.addEventListener('change', function (e) {
+        puzzleTrigger(e.target);
+    });
+    currentSlider.addEventListener('input', function () {
+        displayValue(currentSlider.previousElementSibling, currentSlider);
+    });
 })
-// suwaki.forEach addEventListener('change', () => {
-//     if ((suwakTemperatury.value == correctTemperatureValue) && (suwakTlenu.value == correctOxygeneValue) && (suwakCisnienia.value == correctPressureValue) && (suwakGrawitacji.value == correctGravityValue)) {
-//         console.log("zadanie wykonane");
-//     }
-// })
